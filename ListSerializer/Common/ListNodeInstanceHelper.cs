471ac0032b34c7ab414b372abadd5e0de375af01
Vjacheslaw <dezertir.alkmaara@gmail.com>
2021-04-17 03:29:01 +0300

    feat(ListSerializerJob): create random list from iteration benchmark data

---
+++ ListSerializer/Common/ListNodeInstanceHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class ListNodeInstanceHelper
+    {
+        /// <summary>
+        /// Create random ListNode
+        /// </summary>
+        /// <param name="depth">depth created ListNode</param>
+        /// <remarks>execution speed is not important: method for tests</remarks>
+        public static ListNode CreateRandomListNode(int depth)
+        {
+            var head = new ListNode
+            {
+                Data = "Head"
+            };
+            var previus = head;
+
+            var listAllNodes = new List<ListNode>(depth) {head};
+
+            for (int i = 0; i < depth; i++)
+            {
+                var next = new ListNode { Data = $"Node № {i + 1}" };
+                previus.Next = next;
+                next.Previous = previus;
+                previus = next;
+
+                if (i + 1 == depth)
+                {
+                    next.Data = "Tail";
+                }
+
+                listAllNodes.Add(next);
+            }
+
+            var simpleRandomaiser = new Random();
+            foreach (var node in listAllNodes)
+            {
+                var randomIndex = simpleRandomaiser.Next(0, depth + 1);
+                node.Random = listAllNodes[randomIndex];
+            }
+
+            return head;
+        }
+    }
+}
--- ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
+++ ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
@@ -18,20 +18,7 @@ namespace ListSerializerBenchmark
         public void Setup()
         {
             _serializer = new ListSerializer.ListSerializer();
-            _head = new ListNode()
-            {
-                Data = "Head"
-            };
-            var next = new ListNode()
-            {
-                Data = "Next", 
-                Previous = _head
-            };
-            var next2 = new ListNode() { Data = "Next2", Previous = next, Random = next};
-            next.Next = next2;
-            next.Random = next2;
-            _head.Next = next;
-            _head.Random = next;
+            _head = ListNodeInstanceHelper.CreateRandomListNode(Size);
         }
 
         [Benchmark]
