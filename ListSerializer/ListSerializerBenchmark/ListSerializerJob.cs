512c9044e3674b85f6b20167e6fce9a473f65418
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-05-30 03:17:04 +0300

    update: - update .NET to .NET 6 + update all nuget's

--- ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
+++ ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
@@ -5,7 +5,7 @@ using ListSerializer;
 
 namespace ListSerializerBenchmark
 {
-    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    [SimpleJob(RuntimeMoniker.Net60)]
     public class ListSerializerJob
     {
         [Params(100, 1000, 10000, 100000, 1000000, 10000000)]
@@ -13,13 +13,11 @@ namespace ListSerializerBenchmark
 
         private ListNode _head;
         private IListSerializer _serializer;
-        private IListSerializer _serializerWihoutDictionary;
 
         [IterationSetup]
         public void Setup()
         {
             _serializer = new ListSerializer.ListSerializer();
-            _serializerWihoutDictionary = new ListSerializer.ListSerializerWihoutDictionary();
             _head = ListNodeInstanceHelper.CreateRandomListNode(Size);
         }
 
@@ -27,14 +25,6 @@ namespace ListSerializerBenchmark
         public void DeepCopy()
         {
             var deepCopyTask = _serializer.DeepCopy(_head);
-            deepCopyTask.Wait();
-            var head = deepCopyTask.Result;
-        }
-
-        [Benchmark(Baseline = true)]
-        public void DeepCopyWihoutDictionary()
-        {
-            var deepCopyTask = _serializerWihoutDictionary.DeepCopy(_head);
             deepCopyTask.Wait();
             var head = deepCopyTask.Result;
         }
