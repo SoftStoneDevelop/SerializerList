d0175fb8424bb339a7dffaeedeaa6eeed9119be3
Vyacheslav <softstonedevelop@gmail.com>
2023-07-13 04:30:37 +0300

    fix: restore benchmarks

--- ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
+++ ListSerializer/ListSerializerBenchmark/ListSerializerJob.cs
@@ -15,7 +15,6 @@ namespace ListSerializerBenchmark
 
         private ListNode _head;
         private IListSerializer _serializer1;
-        private IListSerializer _serializer11;
         private IListSerializer _serializer2;
         private IListSerializer _serializer3;
 
@@ -23,7 +22,6 @@ namespace ListSerializerBenchmark
         public void Setup()
         {
             _serializer1 = new ListSerializer.ListSerializerV1();
-            _serializer11 = new ListSerializer.ListSerializerV11();
             _serializer2 = new ListSerializer.ListSerializerV2();
             _serializer3 = new ListSerializer.ListSerializerV3();
             _head = ListNodeInstanceHelper.CreateRandomListNode(Size);
@@ -37,28 +35,20 @@ namespace ListSerializerBenchmark
             var head = deepCopyTask.Result;
         }
 
-        [Benchmark(Description = "V11(fastest): single thread algorithm")]
-        public void DeepCopyV11()
+        [Benchmark(Description = "V2(smallest by memory): multi thread algorithm")]
+        public void DeepCopyV2()
         {
-            var deepCopyTask = _serializer11.DeepCopy(_head);
+            var deepCopyTask = _serializer2.DeepCopy(_head);
             deepCopyTask.Wait();
             var head = deepCopyTask.Result;
         }
 
-        //[Benchmark(Description = "V2(smallest by memory): multi thread algorithm")]
-        //public void DeepCopyV2()
-        //{
-        //    var deepCopyTask = _serializer2.DeepCopy(_head);
-        //    deepCopyTask.Wait();
-        //    var head = deepCopyTask.Result;
-        //}
-
-        //[Benchmark(Description = "V3 (compromise): multi thread algorithm)")]
-        //public void DeepCopyV3()
-        //{
-        //    var deepCopyTask = _serializer3.DeepCopy(_head);
-        //    deepCopyTask.Wait();
-        //    var head = deepCopyTask.Result;
-        //}
+        [Benchmark(Description = "V3 (compromise): multi thread algorithm)")]
+        public void DeepCopyV3()
+        {
+            var deepCopyTask = _serializer3.DeepCopy(_head);
+            deepCopyTask.Wait();
+            var head = deepCopyTask.Result;
+        }
     }
 }
